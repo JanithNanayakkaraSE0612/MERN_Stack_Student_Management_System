a245fb76d9c29e73e4dfaa8c557d710205f904cf
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-12-28 23:52:24 +0530

    Update

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const mongoose = require("mongose");
-const BodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 
 const PORT = process.env.PORT || 8070
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    UseCreateIndex: true.
+    (property) useUnifiedTopologyL: Boolean
+    useUnifiedTopologyL: true,
+    useFindAndModify: false
+})
